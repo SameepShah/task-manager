67d4a0c14c983a1b35ef14d0f0ee35bb3e58e2e9
Sameep <samshah07@gmail.com>
2021-03-09 23:38:42 +0530

    Admin Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
-import { MyProfileComponent } from './my-profile/my-profile.component';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    AboutComponent,
-    MyProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
